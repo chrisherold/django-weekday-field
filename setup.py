f85d46ce12a6cb8917709022b543282352b426e7
Matthew Schinckel <matt@schinckel.net>
2012-08-20 17:36:07 +0930

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-weekday-field",
-    version = "1.0",
+    version = "1.0.1",
     description = "Weekday field for django models",
     url = "http://bitbucket.org/schinckel/django-weekday-field/",
     author = "Matthew Schinckel",
