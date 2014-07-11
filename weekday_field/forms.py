5e4089811e3709816e7a9c856547d80c055c7b50
Nader Morshed <morshed.nader@gmail.com>
2014-07-10 19:14:08 -0700

    Updated test_days to catch the bug fixed in the previous commit Use inherited methods in the field classes where appropriate

--- weekday_field/forms.py
+++ weekday_field/forms.py
@@ -14,12 +14,6 @@ class WeekdayFormField(forms.TypedMultipleChoiceField):
           kwargs['widget'] = forms.widgets.SelectMultiple
         super(WeekdayFormField, self).__init__(*args, **kwargs)
 
-    def clean(self, value):
-        if utils.is_str(value):
-            value = [int(i) for i in value.split(',')]
-        value = super(WeekdayFormField, self).clean(value)
-        return value
-
 class AdvancedWeekdayFormField(WeekdayFormField):
     def __init__(self, *args, **kwargs):
         if 'choices' not in kwargs:
@@ -36,17 +30,13 @@ class AdvancedWeekdayFormField(WeekdayFormField):
 
 class BitwiseWeekdayFormField(WeekdayFormField):
     def __init__(self, *args, **kwargs):
-        kwargs['choices'] = [(x[0],x[2]) for x in utils.BITWISE_DAY_CHOICES]
+        kwargs['choices'] = [(x[0], x[2]) for x in utils.BITWISE_DAY_CHOICES]
         if 'short' in kwargs:
             if kwargs['short']:
-                kwargs['choices'] = [(x[0],x[1]) for x in utils.BITWISE_DAY_CHOICES]
+                kwargs['choices'] = [(x[0], x[1]) for x in utils.BITWISE_DAY_CHOICES]
             del kwargs['short']
         super(BitwiseWeekdayFormField, self).__init__(*args, **kwargs)
 
-    def clean(self,value):
-        value = [int(x) for x in value]
-        if len(value) != 0:
-            value = functools.reduce(operator.or_, value)
-        else:
-            value = 0
-        return value
+    def clean(self, value):
+        value = super(BitwiseWeekdayFormField, self).clean(value)
+        return functools.reduce(operator.or_, [int(x) for x in value], 0)
