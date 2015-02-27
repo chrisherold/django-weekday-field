a94a05c8a9c36083159ad5c948e04c9fd7ca9186
Francesc Pinyol Margalef <francesc.pinyol.m@gmail.com>
2015-02-27 12:37:15 +0100

    added i18n; added Catalan translation

--- weekday_field/utils.py
+++ weekday_field/utils.py
@@ -1,27 +1,29 @@
+from django.utils.translation import ugettext_lazy as _
+
 DAY_CHOICES = (
-    (0, "Monday"),
-    (1, "Tuesday"),
-    (2, "Wednesday"),
-    (3, "Thursday"),
-    (4, "Friday"),
-    (5, "Saturday"),
-    (6, "Sunday")
+    (0, _("Monday")),
+    (1, _("Tuesday")),
+    (2, _("Wednesday")),
+    (3, _("Thursday")),
+    (4, _("Friday")),
+    (5, _("Saturday")),
+    (6, _("Sunday"))
 )
 
 BITWISE_DAY_CHOICES = (
-    (1, "Su","Sunday"),
-    (2, "M","Monday"),
-    (4, "Tu","Tuesday"),
-    (8, "W","Wednesday"),
-    (16, "Th","Thursday"),
-    (32, "F","Friday"),
-    (64, "Sa","Saturday"),
+    (1, "Su",_("Sunday")),
+    (2, "M",_("Monday")),
+    (4, "Tu",_("Tuesday")),
+    (8, "W",_("Wednesday")),
+    (16, "Th",_("Thursday")),
+    (32, "F",_("Friday")),
+    (64, "Sa",_("Saturday")),
 )
 
 ADVANCED_DAY_CHOICES = (
-    ("0,1,2,3,4,5,6", "Any day"),
-    ("0,1,2,3,4", "Weekdays"),
-    ("5,6", "Weekends"),
+    ("0,1,2,3,4,5,6", _("Any day")),
+    ("0,1,2,3,4", _("Weekdays")),
+    ("5,6", _("Weekends")),
 ) + DAY_CHOICES
 
 def is_str(obj):
